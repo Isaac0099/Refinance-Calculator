7e8b54972c3cb383b1ca796fae2fd5869c52fb2a
Isaac0099 <isaacchamberlain01@gmail.com>
2024-12-06 17:59:15 -0700

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import RefinanceCalculator from '@/components/RefinanceCalculator'
+
+export default function Home() {
+  return (
+    <main className="min-h-screen p-8">
+      <RefinanceCalculator />
+    </main>
+  )
+}
